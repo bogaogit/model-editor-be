26a007d5c61711a2dfa710ff6321cbc2ff4937da
Bo Gao <gaobobuq@gmail.com>
2023-11-12 15:36:30 +1000

    Revert "Auxiliary commit to revert individual files from b283d3dbf5c76f2543f1aef78d83bd180936e420"
    This reverts commit d2f3ae03295bcaf3081b06c5e49c14ba41a80675.

--- src/services/ApplicationModel.service.ts
+++ src/services/ApplicationModel.service.ts
@@ -1,21 +1,48 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { ApplicationModel } from "../models/ApplicationModel.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ApplicationModel } from '../models/ApplicationModel.entity';
+import { MetricCollector } from "model-logging";
+import { CloudWatchMetricUnit } from "model-logging/cloudwatch/CloudWatchMetricStore";
 
 @Injectable()
 export class ApplicationModelsService {
   constructor(
     @InjectRepository(ApplicationModel)
     private applicationModelsRepository: Repository<ApplicationModel>,
+    private readonly metricCollector: MetricCollector
   ) {}
+
+  private captureOptional(
+    metric: string,
+    value: number | undefined | null,
+    units: CloudWatchMetricUnit,
+    tags: Record<string, string> = {},
+    namespace: string | undefined = "runtime_recorder",
+  ): void {
+    if (value) {
+      this.metricCollector.capture(metric, value, units, tags, namespace)
+    }
+  }
+
+  private captureStorageMetrics(value: number, prefix: string, tags: Record<string, string>): void {
+    this.captureOptional(prefix + 'storage:current-recording-size', value, 'Bytes', tags)
+    // this.captureOptional(prefix + 'storage:storage-free', metrics.freeStorageBytes, 'bytes', tags)
+    // this.captureOptional(prefix + 'storage:storage-used', metrics.localStorageUsedBytes, 'bytes', tags)
+  }
 
   findAll(): Promise<ApplicationModel[]> {
     return this.applicationModelsRepository.find();
   }
 
   findOne(id: string): Promise<ApplicationModel | null> {
-    return this.applicationModelsRepository.findOneBy({ id });
+    const tags: Record<string, string> = {
+      tag1: "tag1"
+    }
+
+    this.captureStorageMetrics(Math.random(), "prefix", tags)
+    // return this.applicationModelsRepository.findOneBy({ id });
+    return null
   }
 
   async delete(id: string): Promise<void> {
