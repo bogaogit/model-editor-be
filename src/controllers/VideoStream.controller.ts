ec890c646f305ac446c3becf99079839834ce5a0
Bo Gao <gaobobuq@gmail.com>
2023-11-29 21:21:55 +1000

    generate hls and screenshot

--- src/controllers/VideoStream.controller.ts
+++ src/controllers/VideoStream.controller.ts
@@ -8,11 +8,24 @@ import * as fs from "fs";
 
 const ffmpegStatic = require("ffmpeg-static");
 ffmpeg.setFfmpegPath(ffmpegStatic);
+const path = require('path');
 
 /*
 ffmpeg -f dshow -i video="USB Video Device":audio="Microphone (USB Audio Device)"  -f hls -hls_time 1.00 -hls_list_size 0 -hls_segment_filename "D:\repo\model-editor-be\uploads\hls\output_%03d.ts" D:\repo\model-editor-be\uploads\hls\output.m3u8
 
  */
+
+function createPathRecursively(dirPath) {
+  const normalizedPath = path.normalize(dirPath);
+
+  normalizedPath.split(path.sep).reduce((currentPath, folder) => {
+    currentPath += folder + path.sep;
+    if (!fs.existsSync(currentPath)) {
+      fs.mkdirSync(currentPath);
+    }
+    return currentPath;
+  }, '');
+}
 
 @Controller("hls")
 export class StreamController {
@@ -83,13 +96,11 @@ export class StreamController {
 
   @Post("screenshots")
   async startScreenshots() {
-    const inputFilePath = 'E:\\h12';
-    const fileName = "11446468-480p"
+    const inputFilePath = 'E:\\h14';
+    const fileName = "2814829-480p"
 
-    const outputFolderPath = `uploads/screenshots/${fileName}`
-    if (!fs.existsSync(outputFolderPath)){
-      fs.mkdirSync(outputFolderPath);
-    }
+    const outputFolderPath = `uploads/converted/${fileName}/screenshots`
+    createPathRecursively(outputFolderPath);
 
     /**
      * Here,
@@ -100,18 +111,73 @@ export class StreamController {
      * Based on this not operation, the select filter picks up a frame.
      */
 
+    /**
+     *
+     * Replace "input.mp4" with the name of your input video file. The `-vf "fps=1/5"` option sets the frame rate to 1 frame per 5 seconds, indicating that a screenshot will be taken every 5 seconds. Adjust the value if you want screenshots at a different interval.
+     *
+     * The `-q:v 2` option sets the quality of the output images. A lower value (e.g., 2) indicates higher quality, while a higher value (e.g., 10) indicates lower quality. You can adjust this value based on your preference.
+     */
+    let totalTime
     ffmpeg()
       .input(`${inputFilePath}/${fileName}.mp4`)
       .outputOptions([
-        '-vf', 'select=\'not(mod(n,100))\',setpts=\'N/(30*TB)\'',
-        '-f', 'image2'
+        '-vf', 'fps=1/2',
+        '-q:v', '0'
       ])
-      .output(`${outputFolderPath}/thumbnail%03d.jpg`)
+      .output(`${outputFolderPath}/${fileName}-%03d.jpg`)
+      .on('progress', progress => {
+        // HERE IS THE CURRENT TIME
+        const time = parseInt(progress.timemark.replace(/:/g, ''))
+
+        // AND HERE IS THE CALCULATION
+        const percent = (time / totalTime) * 100
+
+        console.log(percent)
+      })
       .on('end', () => {
         console.log('Screenshots generated successfully.');
       })
       .on('error', (err) => {
         console.error('Error generating screenshots:', err);
+      })
+      .run()
+  }
+
+  @Post("convertToHls")
+  async convertToHls() {
+    const inputFilePath = 'E:\\h14';
+    const fileName = "2814829-480p"
+
+    const outputFolderPath = `uploads/converted/${fileName}/hls`
+    createPathRecursively(outputFolderPath);
+
+    let totalTime
+    ffmpeg()
+      .input(`${inputFilePath}/${fileName}.mp4`)
+      .outputOptions([
+        '-c:v', 'libx264',
+        '-crf', '21',
+        '-preset', 'veryfast',
+        '-c:a', 'aac',
+        '-b:a', '128k',
+        '-hls_time', '10',
+        '-hls_list_size', '0'
+      ])
+      .output(`${outputFolderPath}/${fileName}.m3u8`)
+      .on('progress', progress => {
+        // HERE IS THE CURRENT TIME
+        const time = parseInt(progress.timemark.replace(/:/g, ''))
+
+        // AND HERE IS THE CALCULATION
+        const percent = (time / totalTime) * 100
+
+        console.log(percent)
+      })
+      .on('end', () => {
+        console.log('hls generated successfully.');
+      })
+      .on('error', (err) => {
+        console.error('Error generating hls:', err);
       })
       .run()
   }
