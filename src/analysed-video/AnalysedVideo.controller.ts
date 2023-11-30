52478e99a05b34f47afe08f5102e04add5d406d0
Bo Gao <gaobobuq@gmail.com>
2023-11-30 21:21:42 +1000

    c

--- src/analysed-video/AnalysedVideo.controller.ts
+++ src/analysed-video/AnalysedVideo.controller.ts
@@ -1,12 +1,6 @@
 import { Controller, Get, Param } from "@nestjs/common";
-import * as fs from "fs";
-import ffmpeg from "fluent-ffmpeg";
 import { AnalysedVideoService } from "./AnalysedVideo.service";
-
-const ffmpegStatic = require("ffmpeg-static");
-ffmpeg.setFfmpegPath(ffmpegStatic);
-
-type ProcessType = "all" | "hls" | "img" | "audio"
+import { promises as fsPromises } from "fs";
 
 @Controller("analysed-video")
 export class AnalysedVideoController {
@@ -16,10 +10,12 @@ export class AnalysedVideoController {
   }
 
   @Get("images-info/:name")
-  getImagesInfo(@Param("name") name: string) {
-    const folderPath = `uploads/converted/${name}/screenshots`;
-    fs.readdir(folderPath, (err, files) => {
-      console.log(files);
-    });
+  async getImagesInfo(@Param("name") name: string): Promise<string[]> {
+    const directoryPath = `uploads/converted/${name}/screenshots`;
+    try {
+      return await fsPromises.readdir(directoryPath);
+    } catch (error) {
+      throw new Error('Failed to read directory');
+    }
   }
 }
