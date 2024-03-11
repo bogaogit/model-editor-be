f486b8b1a671b7caddcf859724e2f70b9c6033dd
Bo Gao <gaobobuq@gmail.com>
2024-03-11 13:15:08 +1000

    c

--- src/video/analysed-video/AnalysedVideo.controller.ts
+++ src/video/analysed-video/AnalysedVideo.controller.ts
@@ -2,15 +2,13 @@ import { Controller, Get, HttpStatus, Param, Res, Response } from "@nestjs/commo
 import { AnalysedVideoService } from "./AnalysedVideo.service";
 import fs, { promises as fsPromises } from "fs";
 import { VideoIndexInfo } from "../file-scan/FileScan.model";
-import { FileScanService } from "../file-scan/FileScan.service";
 
 const { BardAPI } = require('bard-api-node');
 
 @Controller("analysed-video")
 export class AnalysedVideoController {
   constructor(
-    private readonly analysedVideoService: AnalysedVideoService,
-    private readonly fileScanService: FileScanService
+    private readonly analysedVideoService: AnalysedVideoService
   ) {
   }
 
@@ -50,8 +48,9 @@ export class AnalysedVideoController {
     }
 
     try {
-      const fileEntity = await this.fileScanService.findOneByName(name)
-      videoIndexInfo.videoInfo = fileEntity.videoInfo
+      const metadata = await this.analysedVideoService.getVideoInfo(name)
+      console.log(metadata)
+      videoIndexInfo.videoInfo = await this.analysedVideoService.getVideoInfo(name)
     } catch (error) {
       console.log('Error using ffprobe module:', error);
     }
--- src/video/file-scan/FileScan.module.ts
+++ src/video/file-scan/FileScan.module.ts
@@ -3,12 +3,10 @@ import { FileScanController } from "./FileScan.controller";
 import { FileScanService } from "./FileScan.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConvertedFileInfo } from "./FileScan.entity";
-import { AnalysedVideoModule } from "../analysed-video/AnalysedVideo.module";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ConvertedFileInfo]),
-    AnalysedVideoModule
   ],
   providers: [FileScanService],
   controllers: [FileScanController],
