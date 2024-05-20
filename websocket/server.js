158dafd3de1cfa1f60a5f5c01494bee8bc84bef2
Bo Gao <bogaogit@gmail.com>
2024-05-21 08:25:18 +1000

    update websocket

--- websocket/server.js
+++ websocket/server.js
@@ -1,41 +1,93 @@
 const WebSocket = require("ws");
+const { randomUUID } = require("crypto");
 
 const wss = new WebSocket.Server({ port: 8080 }); // Replace 8080 with your desired port
-const stateUpdateActions = [];
-const clients = [];
-const modelData = {
-  entityData: undefined,
-  applicationModel: undefined
-};
+let webSocketStoredActionList = []
+let rooms = []
 
 wss.on("connection", (ws) => {
   console.log("New client connected");
-  clients.push(ws);
 
   ws.on("message", (message) => {
     console.log("Client message:", message.toString());
-    const data = JSON.parse(message.toString());
 
-    switch (data.type) {
-      case "fetchModel":
+    const body = JSON.parse(message.toString() ?? "{}")
+
+    const {roomId, currentActionId, roomInfo, joinRoomId, webSocketAction, userInfo} = body
+
+    switch (webSocketAction?.actionType) {
+      case 'createRoom':
+        const newRoom = {
+          id: randomUUID(),
+          ...roomInfo,
+        }
+
+        rooms.push(newRoom)
+
         ws.send(JSON.stringify({
-          type: "fetchModel",
-          data: {
-            entityData: modelData.entityData,
-            applicationModel: modelData.applicationModel,
-            actions: stateUpdateActions,
-          }
+            type: "createRoom",
+            room: newRoom,
+            userInfo: {
+              userId: randomUUID(),
+              userName: randomUUID(),
+            },
         }))
-        break;
-      case "loadModel":
-        modelData.entityData = data.data.entityData;
-        modelData.applicationModel = data.data.applicationModel;
-        console.log("Model loaded");
-        break;
+        break
+      case 'getRoomList':
+        ws.send(JSON.stringify({
+            type: "getRoomList",
+            roomList: rooms
+        }))
+        break
+      case 'joinRoom':
+        let room
+        // if (joinRoomId){
+        //   room = rooms.find(e => e.id === joinRoomId)
+        // } else {
+        room = rooms[0]
+        // }
+
+        ws.send(JSON.stringify({
+            type: "joinRoom",
+            userInfo: {
+              userId: randomUUID(),
+              userName: randomUUID(),
+            },
+            room,
+            actionList: webSocketStoredActionList.filter(e => e.roomId === room.id)
+        }))
+        break
+      case 'updateEntity':
+        webSocketStoredActionList.push({
+          id: randomUUID(),
+          roomId: body.roomId,
+          webSocketAction: webSocketAction,
+          userInfo: userInfo
+        })
+        break
+      case 'syncUpdates':
+        break
+      case 'clearActionList':
+        webSocketStoredActionList = []
+        rooms = []
+        break
       default:
-        stateUpdateActions.push(data);
-        clients.forEach(client => client.send(message.toString()));
+        console.log('unknown')
     }
+
+    let actionList = []
+    if (!!currentActionId){
+      const webSocketStoredActionListForRoom = webSocketStoredActionList.filter(e => e.roomId === roomId)
+      const index = webSocketStoredActionListForRoom.findIndex(e => e.id === currentActionId)
+      actionList = webSocketStoredActionListForRoom.slice(index + 1, webSocketStoredActionListForRoom.length)
+    } else {
+      actionList = webSocketStoredActionList.filter(e => e.roomId === roomId)
+    }
+
+    ws.send(JSON.stringify({
+        type: "syncUpdates",
+        actionList
+    }))
   });
 
   ws.on("close", () => {
